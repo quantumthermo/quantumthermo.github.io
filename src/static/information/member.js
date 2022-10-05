f8ac4d9689c6f1a340ceb5fb35525c48a68760aa
quantumthermo <95474282+quantumthermo@users.noreply.github.com>
2022-10-05 13:02:08 +0800

    Update member.js

--- src/static/information/member.js
+++ src/static/information/member.js
@@ -1,10 +1,43 @@
 const member = [
   {
     id: '001',
-    name: 'Guo-Hua Xu',
+    name: 'Guo-Hua Xu (徐国华)',
     avatar: '',
     status: 'PhD student',
     introduction: '',
     email: 'guohuax@zju.edu.cn'
+  }
+]
+
+const member = [
+  {
+    id: '002',
+    name: 'Teng Xiao (肖腾)',
+    avatar: '',
+    status: 'PhD student',
+    introduction: '',
+    email: 'tengxiaozju@zju.edu.cn'
+  }
+]
+
+const member = [
+  {
+    id: '003',
+    name: 'Guitao Lyu (吕桂桃)',
+    avatar: '',
+    status: 'PhD (graduated)',
+    introduction: '',
+    email: ''
+  }
+]
+
+const member = [
+  {
+    id: '004',
+    name: 'Chao Jiang (姜超)',
+    avatar: '',
+    status: 'PhD (graduated)',
+    introduction: '',
+    email: ''
   }
 ]
