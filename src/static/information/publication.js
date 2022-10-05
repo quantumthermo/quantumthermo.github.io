3effcbae405e16ee09bf59a775ed36a4f404831f
quantumthermo <95474282+quantumthermo@users.noreply.github.com>
2022-10-05 14:19:43 +0800

    Update publication.js

--- src/static/information/publication.js
+++ src/static/information/publication.js
@@ -16,18 +16,27 @@ const publications = {
       author: "Gentaro Watanabe, B. Prasanna Venkatesh, Peter Talkner, and Adolfo del Campo",
       title: "Quantum Performance of Thermal Machines over Many Cycles",
       magzine: "Phys. Rev. Lett. 118, 050601 (2017)",
-      magzineUrl: "",
+      magzineUrl: "https://doi.org/10.1103/PhysRevLett.118.050601",
       arXiv: "1612.05586 [Editors' Suggestion; features in APS Physics Synopsis]",
-      arXivUrl: ''
+      arXivUrl: 'https://doi.org/10.48550/arXiv.1612.05586'
     },
     {
       id: "003",
       author: "Gentaro Watanabe and C. J. Pethick",
       title: "Superfluid Density of Neutrons in the Inner Crust of Neutron Stars: New Life for Pulsar Glitch Models",
       magzine: "Phys. Rev. Lett. 119, 062701 (2017)",
-      magzineUrl: "",
+      magzineUrl: "https://doi.org/10.1103/PhysRevLett.119.062701",
       arXiv: "1704.08859",
-      arXivUrl: ''
+      arXivUrl: 'https://doi.org/10.48550/arXiv.1704.08859'
+    },
+    {
+      id: "004",
+      author: "Gentaro Watanabe, B. Prasanna Venkatesh, and Peter Talkner",
+      title: "Generalized energy measurements and modified transient quantum fluctuation theorems",
+      magzine: "Phys. Rev. E 89, 052116 (2014)",
+      magzineUrl: "https://doi.org/10.1103/PhysRevE.89.052116",
+      arXiv: "1402.5704",
+      arXivUrl: 'https://doi.org/10.48550/arXiv.1402.5704'
     }
   ],
 
@@ -40,39 +49,74 @@ const publications = {
       magzine: "",
       magzineUrl: "",
       arXiv: "2205.10742",
-      arXivUrl: ''
+      arXivUrl: 'https://doi.org/10.48550/arXiv.2205.10742'
     },
     {
       id: "002",
-      author: "Guo-Hua Xu and Gentaro Watanabe",
-      title: "Correlation-enhanced stability of microscopic cyclic heat engines",
+      author: "Guo-Hua Xu, Chao Jiang, Yuki Minami, and Gentaro Watanabe",
+      title: "Relation between fluctuations and efficiency at maximum power for small heat engines",
       magzine: "",
       magzineUrl: "",
-      arXiv: "2111.09508",
-      arXivUrl: ''
+      arXiv: "2204.09939",
+      arXivUrl: 'https://doi.org/10.48550/arXiv.2204.09939'
     },
     {
       id: "003",
-      author: "",
-      title: "",
+      author: "Kosuke Ito and Gentaro Watanabe",
+      title: "Collectively enhanced high-power and high-capacity charging of quantum batteries via quantum heat engines",
       magzine: "",
       magzineUrl: "",
-      arXiv: "",
-      arXivUrl: ''
+      arXiv: "2008.07089",
+      arXivUrl: 'https://doi.org/10.48550/arXiv.2008.07089'
+    },
+    {
+      id: "004",
+      author: "Kosuke Ito, Chao Jiang, and Gentaro Watanabe",
+      title: "Universal Bounds for Fluctuations in Small Heat Engines",
+      magzine: "",
+      magzineUrl: "",
+      arXiv: "1910.08096",
+      arXivUrl: 'https://doi.org/10.48550/arXiv.1910.08096'
     }
   ],
 
   // Recent Publications
   recent: [
     {
-      
       id: "001",
-      author: "Guo-Hua Xu, Chao Jiang, Yuki Minami, and Gentaro Watanabe",
-      title: "Relation between fluctuations and efficiency at maximum power for small heat engines",
-      magzine: "",
-      magzineUrl: "",
-      arXiv: "2204.09939",
-      arXivUrl: ''
+      author: "Guo-Hua Xu and Gentaro Watanabe",
+      title: "Correlation-enhanced stability of microscopic cyclic heat engines",
+      magzine: "Phys. Rev. Research 4, L032017 (2022)",
+      magzineUrl: "https://doi.org/10.1103/PhysRevResearch.4.L032017",
+      arXiv: "2111.09508",
+      arXivUrl: 'https://arxiv.org/abs/2111.09508'
+    },
+    {
+      id: "002",
+      author: "Guitao Lyu and Gentaro Watanabe",
+      title: "Persistent current by a static non-Hermitian ratchet",
+      magzine: "Phys. Rev. A 105, 023328 (2022)",
+      magzineUrl: "https://doi.org/10.1103/PhysRevA.105.023328",
+      arXiv: "2110.13773",
+      arXivUrl: 'https://doi.org/10.48550/arXiv.2110.13773'
+    },
+    {
+      id: "003",
+      author: "Gentaro Watanabe and Yuki Minami",
+      title: "Finite-Time Thermodynamics of Fluctuations in Microscopic Heat Engines",
+      magzine: "Phys. Rev. Research 4, L012008 (2022)",
+      magzineUrl: "https://doi.org/10.1103/PhysRevResearch.4.L012008",
+      arXiv: "2108.08602",
+      arXivUrl: 'https://arxiv.org/abs/2108.08602'
+    },
+    {
+      id: "004",
+      author: "Chao Jiang and Gentaro Watanabe",
+      title: "Application of simultaneous and continuous measurement of noncommutative observables: Preparation of the pure ideal quadrature-squeezed state by feedback control",
+      magzine: "Phys. Rev. A 105, 022613 (2022)",
+      magzineUrl: "https://doi.org/10.1103/PhysRevA.105.022613",
+      arXiv: "2105.14945",
+      arXivUrl: "https://doi.org/10.48550/arXiv.2105.14945"
     }
   ]
 }
