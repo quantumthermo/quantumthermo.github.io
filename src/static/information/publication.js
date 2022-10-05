ea9b7fde89de3f57fb577fd637da2678635eefa3
quantumthermo <95474282+quantumthermo@users.noreply.github.com>
2022-10-05 13:52:46 +0800

    Update publication.js

--- src/static/information/publication.js
+++ src/static/information/publication.js
@@ -4,15 +4,24 @@ const publications = {
   representative: [
     {
       id: "001",
+      author: "Gentaro Watanabe, B. Prasanna Venkatesh, Peter Talkner, Myung-Joong Hwang, and Adolfo del Campo",
+      title: "Quantum Statistical Enhancement of the Collective Performance of Multiple Bosonic Engines",
+      magzine: "Phys. Rev. Lett. 124, 210603 (2020)",
+      magzineUrl: "https://doi.org/10.1103/PhysRevLett.124.210603",
+      arXiv: "1904.07811",
+      arXivUrl: 'https://doi.org/10.48550/arXiv.1904.07811'
+    },
+    {
+      id: "002",
       author: "Gentaro Watanabe, B. Prasanna Venkatesh, Peter Talkner, and Adolfo del Campo",
       title: "Quantum Performance of Thermal Machines over Many Cycles",
       magzine: "Phys. Rev. Lett. 118, 050601 (2017)",
       magzineUrl: "",
-      arXiv: "161205586 [Editors' Suggestion; features in APS Physics Synopsis]",
+      arXiv: "1612.05586 [Editors' Suggestion; features in APS Physics Synopsis]",
       arXivUrl: ''
     },
     {
-      id: "002",
+      id: "003",
       author: "Gentaro Watanabe and C. J. Pethick",
       title: "Superfluid Density of Neutrons in the Inner Crust of Neutron Stars: New Life for Pulsar Glitch Models",
       magzine: "Phys. Rev. Lett. 119, 062701 (2017)",
@@ -24,12 +33,47 @@ const publications = {
 
   //Submitted Papers
   submitted: [
-
+    {
+      id: "001",
+      author: "Yuki Minami and Gentaro Watanabe",
+      title: "Effects of pairing gap and band gap on superfluid density in the inner crust of neutron stars",
+      magzine: "",
+      magzineUrl: "",
+      arXiv: "2205.10742",
+      arXivUrl: ''
+    },
+    {
+      id: "002",
+      author: "Guo-Hua Xu and Gentaro Watanabe",
+      title: "Correlation-enhanced stability of microscopic cyclic heat engines",
+      magzine: "",
+      magzineUrl: "",
+      arXiv: "2111.09508",
+      arXivUrl: ''
+    },
+    {
+      id: "003",
+      author: "",
+      title: "",
+      magzine: "",
+      magzineUrl: "",
+      arXiv: "",
+      arXivUrl: ''
+    }
   ],
 
   // Recent Publications
   recent: [
-
+    {
+      
+      id: "001",
+      author: "Guo-Hua Xu, Chao Jiang, Yuki Minami, and Gentaro Watanabe",
+      title: "Relation between fluctuations and efficiency at maximum power for small heat engines",
+      magzine: "",
+      magzineUrl: "",
+      arXiv: "2204.09939",
+      arXivUrl: ''
+    }
   ]
 }
 export default publications
