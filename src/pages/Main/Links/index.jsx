c25c6c9653860acc472cba7a0120e634d16a3693
oldfish1996 <2318587805@qq.com>
2022-10-25 20:56:29 +0800

    update

--- src/pages/Main/Links/index.jsx
+++ src/pages/Main/Links/index.jsx
@@ -5,6 +5,15 @@ export default function Links() {
   return (
     <section className='links'>
       <p className='nav-item-title'>LINKS</p>
+      <div className='links-content'>
+        <ul>
+          <li>
+            <a rel="noreferrer" target="_blank" href="https://person.zju.edu.cn/gentaro/">
+              Personal homepage of teachers of Zhejiang University(浙江大学教师个人主页)
+            </a>
+          </li>
+        </ul>
+      </div>
     </section>
   )
 }
