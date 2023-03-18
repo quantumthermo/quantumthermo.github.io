7bdd75846a2c3549ef43d9c08c9e43bb6d4a2c67
oldfish1996 <2318587805@qq.com>
2023-03-18 21:57:25 +0800

    update

--- src/pages/Main/Links/index.jsx
+++ src/pages/Main/Links/index.jsx
@@ -1,17 +1,39 @@
-import React from 'react'
-import './index.css'
+import React, { useEffect, useState } from 'react';
+import links from '../../../static/information/links';
+import './index.css';
 
 export default function Links() {
+
+  const [lks, setLks] = useState(links);
+
+  useEffect(() => {
+    fetch('https://oldfish1996.github.io/quantumthermo-data/links.json')
+    .then(res => res.json())
+    .then(json => {
+      const {links: fecthLink} = json;
+      setLks(fecthLink)
+      console.log(fecthLink);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+  }, []);
+
+
   return (
     <section className='links'>
       <p className='nav-item-title'>LINKS</p>
       <div className='links-content'>
         <ul>
-          <li>
-            <a rel="noreferrer" target="_blank" href="https://person.zju.edu.cn/gentaro/">
-              Personal homepage of teachers of Zhejiang University(浙江大学教师个人主页)
-            </a>
-          </li>
+          {
+            lks.map((li, idx) => (
+              <li key={idx+li.id}>
+                <a rel="noreferrer" target="_blank" href={li.url}>
+                  {li.content}
+                </a>
+              </li>
+            ))
+          }
         </ul>
       </div>
     </section>
---
+++ src/static/information/links.js
@@ -0,0 +1,9 @@
+const links = [
+  {
+    "id": "001",
+    "content": "Personal homepage of teachers of Zhejiang University(浙江大学教师个人主页)",
+    "url": "https://person.zju.edu.cn/gentaro/"
+  }
+];
+
+export default links;
